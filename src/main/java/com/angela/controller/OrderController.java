ba5295f0fe4ae1272eb2e2cef64a9c4dbadea216
Tan Yawen <2749934612@qq.com>
2026-03-24 12:08:43 +0800

    init

---
+++ src/main/java/com/angela/controller/OrderController.java
@@ -0,0 +1,12 @@
+package com.angela.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+    @RequestMapping("/order")
+    public String order() {
+        return "order";
+    }
+}
